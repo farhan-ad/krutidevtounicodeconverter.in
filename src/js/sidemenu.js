66798224729900c7f0fd03adbd414747b2908759
Farhan Ad <farhanad009@gmail.com>
2024-03-11 10:00:24 +0530

    Fixed sidemenu.js for homepage

--- src/js/sidemenu.js
+++ src/js/sidemenu.js
@@ -1,8 +1,23 @@
-// Function to set active link based on current URL
 function setActiveLink() {
-  const currentPath = window.location.href;
+  let currentPath = window.location.href;
+  // Normalize the currentPath to handle the domain or home page without index.html
+  const baseUrl = window.location.origin + "/";
+  const homePageEndings = ["index.html", ""];
+
+  if (homePageEndings.some((ending) => currentPath.endsWith(ending))) {
+    currentPath = baseUrl;
+  }
+
   document.querySelectorAll(".secondary-nav_ul a").forEach((link) => {
-    if (currentPath === link.href) {
+    // Normalize the link href for a direct comparison
+    let linkHref = link.getAttribute("href");
+    if (linkHref === "index.html" || linkHref === "") {
+      linkHref = baseUrl;
+    } else {
+      linkHref = new URL(linkHref, baseUrl).href;
+    }
+
+    if (currentPath === linkHref) {
       link.classList.add("active");
     } else {
       link.classList.remove("active");
@@ -10,5 +25,6 @@ function setActiveLink() {
   });
 }
 
-// Call setActiveLink on page load
+// Call setActiveLink on page load and on history state changes
 window.addEventListener("load", setActiveLink);
+window.addEventListener("popstate", setActiveLink);
